2fbf5d67b941c887c54db341cc6f830da0400ccb
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-07-22 23:03:17 +0900

    2021年 7月22日 木曜日 23時03分17秒 JST

--- conf.d/_invisible_conda_name.fish
+++
@@ -1,4 +0,0 @@
-function _invisible_conda_name -e fish_prompt
-    set -u CONDA_PROMPT_MODIFIER
-end
-
---
+++ functions/invisible_conda_name.fish
@@ -0,0 +1,4 @@
+function invisible_conda_name -e fish_prompt
+    set -u CONDA_PROMPT_MODIFIER
+end
+
