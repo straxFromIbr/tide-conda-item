0ebec16da99edf91c55e33be782ccc327fa6f0b4
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-08-31 09:44:33 +0900

    Tue Aug 31 09:44:33 JST 2021

--- functions/_tide_item_conda_env.fish
+++ functions/_tide_item_conda_env.fish
@@ -1,6 +1,8 @@
 function _tide_item_conda_env
-	if test $CONDA_DEFAULT_ENV && which python|grep mini>/dev/null
-        _tide_print_item conda_env $tide_virtual_env_icon' '$CONDA_DEFAULT_ENV 
+	if  which python|grep forge>/dev/null
+        #echo (which python | string split '/' )[-3] 
+        _tide_print_item conda_env $tide_virtual_env_icon' '(which python | string split '/' )[-3] 
+
     else
         _tide_print_item conda_env ''
 	end
