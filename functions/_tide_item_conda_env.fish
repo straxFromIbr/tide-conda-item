315c598a6eccfe3ed2d5b9e62f421770bf2a880e
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-08-31 09:41:48 +0900

    Tue Aug 31 09:41:48 JST 2021

--- functions/_tide_item_conda_env.fish
+++ functions/_tide_item_conda_env.fish
@@ -1,5 +1,7 @@
 function _tide_item_conda_env
 	if test $CONDA_DEFAULT_ENV && which python|grep mini>/dev/null
         _tide_print_item conda_env $tide_virtual_env_icon' '$CONDA_DEFAULT_ENV 
+    else
+        _tide_print_item conda_env ''
 	end
 end
